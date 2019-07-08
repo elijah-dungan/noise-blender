c65d9a454f656af3ff7b0dcaabc781c106b4095f
root <eli.dungan@gmail.com>
2019-07-08 15:32:03 -0700

    working start and stop button

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 /** credit goes to Johan Karisson https://codepen.io/DonKarlssonSan/post/drum-loops-and-effects-with-web-audio-api, and MDN https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API for example Web Audio API code **/
 
 // global variables
+
 var source = [];
 var audioContext = new AudioContext();
 var gainNode = audioContext.createGain();
@@ -36,65 +37,69 @@ var ogg = [
   }
 ];
 
-// for(var i = 0; i < ogg.length; i ++) {
-//   console.log(ogg[i].name);
-// }
-
 // event handlers
-function start() {
+
+function startSounds() { // starts all audio assets
   for(var i = 0; i < ogg.length; i++) {
     source[i] = audioContext.createBufferSource(); // mp3 player
     source[i].buffer = ogg[i].buffer; // ogg files
     source[i].connect(gainNode).connect(audioContext.destination); // wire and speakers with volume control
     source[i].loop = true;
     source[i].start();
-    source.push[i];
   }
+  playEl.removeEventListener('click', startSounds);
+  playEl.addEventListener('click', stopSounds);
 }
 
-function stop() {
-  source.stop();
+function stopSounds() { // stops all audio assets
+  for(var i = 0; i <ogg.length; i++) {
+    source[i].stop();
+  }
+  playEl.removeEventListener('click', stopSounds);
+  playEl.addEventListener('click', startSounds);
 }
 
 // event listers
-playEl.addEventListener('click', start);
+
+playEl.addEventListener('click', startSounds);
 
 // executables
 
+/* attempted to create for loop but recieved undefined error with ogg array */
 fetch('https://elijah-dungan.github.io/ogg/beach.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // takes completely read audio stream and asynchronously decodes it
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[0].buffer = audioBuffer;
   });
 fetch('https://elijah-dungan.github.io/ogg/waterfall.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // asynchronously decodes completely read audio stream
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[1].buffer = audioBuffer;
   });
 fetch('https://elijah-dungan.github.io/ogg/stream.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // asynchronously decodes completely read audio stream
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[2].buffer = audioBuffer;
   });
 fetch('https://elijah-dungan.github.io/ogg/rain.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // asynchronously decodes completely read audio stream
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[3].buffer = audioBuffer;
   });
 fetch('https://elijah-dungan.github.io/ogg/canopy.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // asynchronously decodes completely read audio stream
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[4].buffer = audioBuffer;
   });
 fetch('https://elijah-dungan.github.io/ogg/frogs.ogg')
   .then(response => response.arrayBuffer()) // takes response stream and reads it to completion
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // asynchronously decodes completely read audio stream
-  .then(audioBuffer => { // represents audio asset?
+  .then(audioBuffer => { // represents audio asset
     ogg[5].buffer = audioBuffer;
   });
 
