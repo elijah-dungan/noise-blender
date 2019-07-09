959e64e782d2154a2612f7854b95c9fbf003e55a
root <eli.dungan@gmail.com>
2019-07-08 18:27:16 -0700

    created simple wireframe layout

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use-strict';
-/** credit goes to Johan Karisson https://codepen.io/DonKarlssonSan/post/drum-loops-and-effects-with-web-audio-api, and MDN https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API for example Web Audio API code **/
+
+/** special thanks goes to Johan Karisson https://codepen.io/DonKarlssonSan/post/drum-loops-and-effects-with-web-audio-api, MDN https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API, and W3 Schools https://www.w3schools.com/howto/howto_js_rangeslider.asp **/
 
 // global variables
 
 var noise = [];
+var gainNode = [];
 var audioContext = new AudioContext();
-var gainNode = audioContext.createGain();
 var playEl = document.getElementById('audio-button-1'); // references button element by ID
 
 // objects
@@ -37,6 +38,9 @@ var ogg = [
   }
 ];
 
+// helper functions
+
+
 // event handlers
 
 function appear() { // makes button visible after 1 second timer, allows executables to finish buffering before user plays audio
@@ -47,16 +51,18 @@ function appear() { // makes button visible after 1 second timer, allows executa
 
 function startSounds() { // starts all audio assets
   for(var i = 0; i < ogg.length; i++) {
-    noise[i] = audioContext.createBufferSource(); // mp3 player
     gainNode[i] = audioContext.createGain(); // creates gain node for each audio asset
-    noise[i].buffer = ogg[i].buffer; // ogg files
-    noise[i].connect(gainNode[i]).connect(audioContext.destination); // wire and speakers with volume control
-    noise[i].loop = true;
-    gainNode[i].gain.value = 0.4; // sets default volume
+    // TODO - need check for local memory and grab user gain values, then for loop the values into the gainNode array
+    gainNode[i].gain.value = 0.5; // sets default volume
+    noise[i] = audioContext.createBufferSource(); // creates audio players
+    noise[i].buffer = ogg[i].buffer; // assigns audio assets to buffers
+    noise[i].connect(gainNode[i]).connect(audioContext.destination); // connects audio assets to volume controls and speakers
+    noise[i].loop = true; // ensures each audio asset will loop
     noise[i].start();
   }
   playEl.removeEventListener('click', startSounds);
   playEl.addEventListener('click', stopSounds);
+  console.log(gainNode, noise);
 }
 
 function stopSounds() { // stops all audio assets
@@ -67,8 +73,11 @@ function stopSounds() { // stops all audio assets
   playEl.addEventListener('click', startSounds);
 }
 
+//TODO need to add gain control event handler, event handler should store gain values into local memory
+
 // event listers
 
+//TODO need to add gain control event listener
 window.addEventListener('load', appear);
 playEl.addEventListener('click', startSounds);
 
