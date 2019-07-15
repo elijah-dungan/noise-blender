6b4fb75b27fe7f1ca45436b28e8cbdab550b4513
codeNinja <49661139+elijah-dungan@users.noreply.github.com>
2019-07-15 04:33:50 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -153,7 +153,6 @@ function enable() { // makes button visible after 1 second timer, allows executa
       playEl.disabled = false;
     }, 1250);
     playEl.addEventListener('click', startSounds);
-    playEl.addEventListener('touchstart', startSounds);
     window.removeEventListener('load', enable);
   }
 }
